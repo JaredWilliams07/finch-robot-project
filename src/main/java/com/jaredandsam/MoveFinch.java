2ac9fb13d76e048a02c51fcf70bebb619437c452
JaredWilliams07 <jhwilliams2007@gmail.com>
2025-06-05 10:43:35 -0700

    tets

--- src/main/java/com/jaredandsam/MoveFinch.java
+++ src/main/java/com/jaredandsam/MoveFinch.java
@@ -5,7 +5,7 @@ import com.birdbrain.Finch;
 public class MoveFinch  {
     public static void main(String[] args){
         Finch myfinch = new Finch();
-    myfinch.setMove("F", 97, 70);
+    myfinch.setMove("F", 100, 70);
     playNoteWhenTurning(myfinch);
     myfinch.setTurn("L", 90, 80);
     myfinch.setMove("F", 36, 70);
@@ -16,7 +16,7 @@ public class MoveFinch  {
     myfinch.setTurn("R", 90, 80);
     myfinch.setMove("F", 55, 70);
     playNoteWhenTurning(myfinch);
-    myfinch.setTurn("R", 90, 80);
+    myfinch.setTurn("R", 94, 80);
     myfinch.setMove("F",120, 70);
     playNoteWhenTurning(myfinch);
     myfinch.setTurn("R", 90, 80);
@@ -27,6 +27,50 @@ public class MoveFinch  {
     playNoteWhenTurning(myfinch);
     myfinch.setTurn("L", 90, 80);
     myfinch.setMove("F",myfinch.getDistance() - 3, 80);
+    //Ending Dance
+    myfinch.setTurn("L", 360, 100);
+    myfinch.setTurn("R", 360, 100);
+    myfinch.playNote(64, 0.5); // E
+    myfinch.setBeak(10,50,81);
+    myfinch.playNote(64, 0.5); // E
+    myfinch.setBeak(32,74,14);
+    myfinch.pause(0.55);
+    myfinch.playNote(65, 0.5); // F
+    myfinch.setBeak(100,90,63);
+    myfinch.pause(0.55);
+    myfinch.playNote(67, 0.5); // G
+    myfinch.setBeak(15,72,31);
+    myfinch.pause(0.55);
+    myfinch.playNote(67, 0.5); // G
+    myfinch.setBeak(5,75,0);
+    myfinch.pause(0.55);
+    myfinch.playNote(65, 0.5); // F
+    myfinch.setBeak(100,69,69);
+    myfinch.pause(0.55);
+    myfinch.playNote(62, 0.5); // D
+    myfinch.setBeak(12,45,74);
+    myfinch.pause(0.55);
+    myfinch.playNote(60, 0.5); // C
+    myfinch.setBeak(69,69,42);
+    myfinch.pause(0.55);
+    myfinch.playNote(60, 0.5); // C
+    myfinch.setBeak(45,10,0);
+    myfinch.pause(0.55);
+    myfinch.playNote(62, 0.5); // D
+    myfinch.setBeak(12,74,44);
+    myfinch.pause(0.55);
+    myfinch.playNote(64, 0.5); // E
+    myfinch.setBeak(85,10,27);
+    myfinch.pause(0.55);
+    myfinch.playNote(64, 0.5); // E
+    myfinch.setBeak(69,45,1);
+    myfinch.pause(0.55);
+    myfinch.playNote(62, 0.5); // D
+    myfinch.setBeak(47,35,75);
+    myfinch.pause(0.55);
+    myfinch.playNote(62, 1.0); // D 
+    myfinch.setBeak(1,1,100);
+    myfinch.pause(1.05);
 
     myfinch.disconnect();
     }
--- src/main/java/com/jaredandsam/Test.java
+++ src/main/java/com/jaredandsam/Test.java
@@ -7,11 +7,59 @@ public class Test {
         Finch myfinch = new Finch();
     System.out.println("Distance: " + myfinch.getDistance());
 
-    // myfinch.playNote(99, 2);
+    // myfinch.setTurn("L", 360, 100);
+    // myfinch.setTurn("R", 360, 100);
+    // myfinch.playNote(64, 0.5); // E
+    // myfinch.setBeak(10,50,81);
+    // myfinch.playNote(64, 0.5); // E
+    // myfinch.setBeak(32,74,14);
+    // myfinch.pause(0.55);
+    // myfinch.playNote(65, 0.5); // F
+    // myfinch.setBeak(100,90,63);
+    // myfinch.pause(0.55);
+    // myfinch.playNote(67, 0.5); // G
+    // myfinch.setBeak(15,72,31);
+    // myfinch.pause(0.55);
+    // myfinch.playNote(67, 0.5); // G
+    // myfinch.setBeak(5,75,0);
+    // myfinch.pause(0.55);
+    // myfinch.playNote(65, 0.5); // F
+    // myfinch.setBeak(100,69,69);
+    // myfinch.pause(0.55);
+    // myfinch.playNote(62, 0.5); // D
+    // myfinch.setBeak(12,45,74);
+    // myfinch.pause(0.55);
+    // myfinch.playNote(60, 0.5); // C
+    // myfinch.setBeak(69,69,42);
+    // myfinch.pause(0.55);
+    // myfinch.playNote(60, 0.5); // C
+    // myfinch.setBeak(45,10,0);
+    // myfinch.pause(0.55);
+    // myfinch.playNote(62, 0.5); // D
+    // myfinch.setBeak(12,74,44);
+    // myfinch.pause(0.55);
+    // myfinch.playNote(64, 0.5); // E
+    // myfinch.setBeak(85,10,27);
+    // myfinch.pause(0.55);
+    // myfinch.playNote(64, 0.5); // E
+    // myfinch.setBeak(69,45,1);
+    // myfinch.pause(0.55);
+    // myfinch.playNote(62, 0.5); // D
+    // myfinch.setBeak(47,35,75);
+    // myfinch.pause(0.55);
+    // myfinch.playNote(62, 1.0); // D 
+    // myfinch.setBeak(1,1,100);
+    // myfinch.pause(1.05);
+
+
+    myfinch.setBeak(0,0,0);
+
+
+
     // myfinch.setBeak(0,0,0);
-    myfinch.setTurn("L", 75, 85);
-    myfinch.setTurn("R", 150, 85);
-    myfinch.setTurn("L", 435, 85);
+    // myfinch.setTurn("L", 75, 85);
+    // myfinch.setTurn("R", 150, 85);
+    // myfinch.setTurn("L", 435, 85);
     myfinch.disconnect();
     }
 }
